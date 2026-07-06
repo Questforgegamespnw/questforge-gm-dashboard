f2f9e466c9d4fcb6c5f4f62f7530dfd85f1d2fc5
Matthew Hughes <questforgegamespnw@gmail.com>
2026-07-06 10:21:38 -0700

    changed current_loadout back to valhalla campaign for weekend session.

--- data/campaigns/current_loadout.js
+++ data/campaigns/current_loadout.js
@@ -1,42 +1,111 @@
 export const currentLoadout = {
-  modeId: "campaign_placeholder",
-  campaignId: "campaign_placeholder",
-  arcId: "vanished_island",
-  sessionId: "session_current",
+  modeId: "valhalla",
+  campaignId: "valhalla",
+  arcId: "arc_ember_root",
+  sessionId: "session_ember_root_act_2",
 
   activeActors: [
-    "actor_reef_raider",
-    "actor_tidecaller",
-    "actor_expedition_sailor"
-  ], 
+    "actor_durgrim_ashenvein",
+    "actor_kaldra_steelmind",
+    "actor_bruni_coalhand",
+    "actor_skarth_firebrand",
+    "actor_thora_kara_emberroot",
+    "actor_forge_guard",
+    "actor_ironhand_engineer",
+    "actor_ashbound_defender",
+    "actor_magma_reaver",
+    "actor_cinder_channeler",
+    "actor_ember_guard"
+  ],
+
+  activeAmbientCast: [
+    "ambient_emberroot_citywide_crisis_crowd",
+    "ambient_emberroot_high_forge_command_staff",
+    "ambient_emberroot_midworks_ironhand_crews",
+    "ambient_emberroot_lower_furnaces_ashbound_workers",
+    "ambient_emberroot_chainways_cross_traffic",
+    "ambient_emberroot_core_furnace_regulator_crews"
+  ],
 
   activeLocations: [
-    "location_cardinal_complex",
-    "location_jungle",
-    "location_island_shore"
+    "location_emberfall_forge_city",
+    "location_emberfall_high_forge",
+    "location_emberfall_midworks",
+    "location_emberfall_lower_furnaces",
+    "location_emberfall_chainways",
+    "location_emberfall_core_furnace"
   ],
 
   activeFactions: [
-    "faction_expedition_sailors",
-    "faction_merfolk"
-  ],
-
-  activeScenes: [
-    "scene_race_to_the_ship"
+    "faction_emberroot_forge_lords",
+    "faction_emberroot_ironhand_guilds",
+    "faction_emberroot_ashbound",
+    "faction_emberroot_fire_giants"
   ],
 
   activeThreads: [
-    "thread_ship_under_attack",
-    "thread_time_dilation",
-    "thread_missing_ships"
+    "thread_emberroot_no_clean_victories",
+    "thread_emberroot_city_breathing_wrong",
+    "thread_emberroot_fire_is_people",
+    "thread_emberroot_stolen_emberlings",
+    "thread_emberroot_control_vs_survival",
+    "thread_emberroot_forge_lord_legitimacy_collapse",
+    "thread_emberroot_safe_deconstruction",
+    "thread_emberroot_consent_transition",
+    "thread_emberroot_root_heat_siphon",
+    "thread_emberroot_valkyrie_strain",
+    "thread_emberroot_hot_exfil_consequence_chain"
   ],
 
-  activeTrackers: [],
+  activeTrackers: [
+    "tracker_emberroot_investigation_clock",
+    "tracker_emberroot_insight",
+    "tracker_emberroot_core_stability",
+    "tracker_emberroot_giant_timer",
+    "tracker_emberroot_unsavable_crises",
+    "tracker_emberroot_exfil_state"
+  ],
+
+  activeScenes: [
+    "scene_emberroot_phase_zero_hot_landing",
+    "scene_emberroot_arrival_evacuate_civilians",
+    "scene_emberroot_arrival_stabilize_structure",
+    "scene_emberroot_arrival_stop_forge_overlord",
+    "scene_emberroot_high_forge_refuge",
+
+    "scene_emberroot_s02_thread_a_system_failure",
+    "scene_emberroot_s02_thread_b_giant_interference",
+    "scene_emberroot_s02_thread_c_root_resonance",
+    "scene_emberroot_realization_shift",
+    "scene_emberroot_core_furnace_descent",
+
+    "scene_emberroot_s03_forge_below_living_core",
+    "scene_emberroot_s03_break_the_chains",
+
+    "scene_emberroot_s04_molten_council",
+    "scene_emberroot_join_skarth_branch",
+
+    "scene_emberroot_s05_unsavable",
+    "scene_emberroot_runaway_transformation_variant",
+    "scene_emberroot_hot_exfil"
+  ],
+
+  activeFireableMoments: [],
 
   activeTables: [],
 
   activeReferences: [
-    "reference_campaign_current_state",
-    "reference_known_vs_hidden_truth"
+    "reference_emberroot_act_overview",
+    "reference_emberroot_truth_dossier",
+    "reference_emberroot_quick_interpretation",
+    "reference_emberroot_difficulty_controls",
+    "reference_emberroot_oh_shit_rules",
+    "reference_emberroot_caste_pressure",
+    "reference_emberroot_consequence_system",
+    "reference_emberroot_encounter_design",
+    "reference_emberroot_fire_giant_motivation",
+    "reference_emberroot_skarth_escalation",
+    "reference_emberroot_caste_system",
+    "reference_emberroot_visual_design_language"
   ]
 };
