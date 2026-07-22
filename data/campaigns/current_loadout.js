388c0cd5c3d87e5d7ed793b3ecb6e6e38e1cf379
Matthew Hughes <questforgegamespnw@gmail.com>
2026-07-22 09:07:39 -0700

    changed current loadout

--- data/campaigns/current_loadout.js
+++ data/campaigns/current_loadout.js
@@ -1,133 +1,100 @@
 export const currentLoadout = {
-  modeId: "valhalla",
-  campaignId: "valhalla",
-  arcId: "arc_ember_root",
-  sessionId: "session_ember_root_act_2_cpp",
+  modeId: "questforge-default",
+  campaignId: "no_bell_for_harthrun",
+  arcId: "harthrun_investigation",
+  sessionId: "one-shot",
 
   activeActors: [
-    "actor_durgrim_ashenvein",
-    "actor_kaldra_steelmind",
-    "actor_bruni_coalhand",
-    "actor_skarth_firebrand",
-    "actor_thora_kara_emberroot",
-    "actor_forge_guard",
-    "actor_ironhand_engineer",
-    "actor_ashbound_defender",
-    "actor_magma_reaver",
-    "actor_cinder_channeler",
-    "actor_ember_guard"
+    "actor_fort_dios_duty_officer",
+    "actor_subsumed_captain",
+    "actor_ichor_demon"
   ],
 
   activeAmbientCast: [
-    "ambient_emberroot_citywide_crisis_crowd",
-    "ambient_emberroot_high_forge_command_staff",
-    "ambient_emberroot_midworks_ironhand_crews",
-    "ambient_emberroot_lower_furnaces_ashbound_workers",
-    "ambient_emberroot_chainways_cross_traffic",
-    "ambient_emberroot_core_furnace_regulator_crews"
+    "ambient_fort_dios_soldiers",
+    "ambient_harthrun_dead",
+    "ambient_captive_villagers",
+    "ambient_converted_thralls"
   ],
 
   activeLocations: [
-    "location_emberfall_forge_city",
-    "location_emberfall_high_forge",
-    "location_emberfall_midworks",
-    "location_emberfall_lower_furnaces",
-    "location_emberfall_chainways",
-    "location_emberfall_core_furnace"
+    "location_fort_dios",
+    "location_harthrun_approach",
+    "location_harthrun_village",
+    "location_harthrun_mine",
+    "location_lower_mine",
+    "location_conversion_chambers"
   ],
 
   activeFactions: [
-    "faction_emberroot_forge_lords",
-    "faction_emberroot_ironhand_guilds",
-    "faction_emberroot_ashbound",
-    "faction_emberroot_fire_giants"
+    "faction_fort_dios_command",
+    "faction_harthrun_villagers",
+    "faction_ichor_legion"
   ],
 
   activeThreads: [
-    "thread_emberroot_no_clean_victories",
-    "thread_emberroot_city_breathing_wrong",
-    "thread_emberroot_root_heat_siphon",
-    "thread_emberroot_fire_is_people",
-    "thread_emberroot_stolen_emberlings",
-    "thread_emberroot_control_vs_survival",
-    "thread_emberroot_crown_preservation_protocol",
-    "thread_emberroot_forge_lord_legitimacy_collapse",
-    "thread_emberroot_emergency_coalition",
-    "thread_emberroot_safe_deconstruction",
-    "thread_emberroot_ashbound_hidden_routes",
-    "thread_emberroot_consent_transition",
-    "thread_emberroot_crown_anvil_intervention",
-    "thread_emberroot_valkyrie_strain",
-    "thread_emberroot_hot_exfil_consequence_chain"
+    "thread_harthrun_request_for_aid",
+    "thread_missing_villagers",
+    "thread_ledger_command_network",
+    "thread_ichor_conversion",
+    "thread_subsumed_captain",
+    "thread_ichor_legion_expansion"
   ],
 
   activeTrackers: [
-    "tracker_emberroot_investigation_clock",
-    "tracker_emberroot_insight",
-    "tracker_emberroot_core_stability",
-    "tracker_emberroot_crown_preservation_protocol",
-    "tracker_emberroot_giant_timer",
-    "tracker_emberroot_ashbound_evacuation",
-    "tracker_emberroot_controlled_decommission",
-    "tracker_emberroot_crown_anvil_pressure",
-    "tracker_emberroot_unsavable_crises",
-    "tracker_emberroot_exfil_state"
+    "tracker_conversion_progress",
+    "tracker_mine_alert",
+    "tracker_ledger_influence"
   ],
 
   activeScenes: [
-    "scene_emberroot_phase_zero_hot_landing",
-    "scene_emberroot_arrival_evacuate_civilians",
-    "scene_emberroot_arrival_stabilize_structure",
-    "scene_emberroot_arrival_stop_forge_overlord",
-    "scene_emberroot_high_forge_refuge",
-
-    "scene_emberroot_s02_thread_c_root_resonance",
-    "scene_emberroot_split_politics_and_lungs",
-    "scene_emberroot_upper_political_pressure",
-    "scene_emberroot_s02_thread_a_system_failure",
-
-    "scene_emberroot_confrontation_crown_preservation_revealed",
-    "scene_emberroot_breakout_command_seals_lock",
-    "scene_emberroot_emergency_coalition_cut_crownlines",
-    "scene_emberroot_giant_coordination_break_right_lines",
-    "scene_emberroot_ashbound_evacuation_hidden_routes",
-    "scene_emberroot_core_furnace_descent",
-    "scene_emberroot_lower_works_controlled_decommission",
-
-    "scene_emberroot_s05_unsavable",
-    "scene_emberroot_runaway_transformation_variant",
-    "scene_emberroot_hot_exfil"
+    "scene_fort_dios_briefing",
+    "scene_harthrun_approach",
+    "scene_village_that_bled",
+    "scene_following_the_taken",
+    "scene_entering_the_mine",
+    "scene_lower_mine",
+    "scene_conversion_chamber",
+    "scene_aftermath"
   ],
 
   activeFireableMoments: [
-    "moment_emberroot_tri_voice_collision",
-    "moment_emberroot_bruni_people_over_systems",
-    "moment_emberroot_kaldra_bad_assumption",
-    "moment_emberroot_valkyrie_capacity_warning",
-    "moment_emberroot_skarth_control_is_the_problem",
-    "moment_emberroot_skarth_council_reframe",
-    "moment_emberroot_lower_works_backflow_surge",
-    "moment_emberroot_realization_same_pressure"
+    "moment_no_smoke_from_harthrun",
+    "moment_first_uniform",
+    "moment_orderly_tracks_disorderly_flight",
+    "moment_fort_dios_reply",
+    "moment_bodies_do_not_balance",
+    "moment_resistance_broke_here",
+    "moment_sorted_not_slaughtered",
+    "moment_blood_trail_to_mine",
+    "moment_wrong_wounds",
+    "moment_bell_never_rang",
+    "moment_thralls_do_not_speak",
+    "moment_captives_were_counted",
+    "moment_allied_command_language",
+    "moment_first_impaled_victim",
+    "moment_removal_is_not_rescue",
+    "moment_critical_window",
+    "moment_captives_awaiting_processing",
+    "moment_captain_answers",
+    "moment_command_link_falters",
+    "moment_demon_reclaims_him",
+    "moment_ledger_writes"
   ],
 
   activeTables: [],
 
   activeReferences: [
-    "reference_emberroot_act_overview",
-    "reference_emberroot_truth_dossier",
-    "reference_emberroot_scene_spine",
-    "reference_emberroot_quick_interpretation",
-    "reference_emberroot_difficulty_controls",
-    "reference_emberroot_oh_shit_rules",
-    "reference_emberroot_crown_preservation_protocol",
-    "reference_emberroot_confrontation_guide",
-    "reference_emberroot_emergency_coalition_roles",
-    "reference_emberroot_controlled_decommission",
-    "reference_emberroot_crown_anvil",
-    "reference_emberroot_fire_giant_motivation",
-    "reference_emberroot_ashbound_routes",
-    "reference_emberroot_encounter_design",
-    "reference_emberroot_consequence_system",
-    "reference_emberroot_visual_design_language"
+    "reference_adventure_spine",
+    "reference_harthrun_investigation",
+    "reference_clue_convergence",
+    "reference_request_reply_chronology",
+    "reference_ledger_command_geas",
+    "reference_ichor_spike_conversion",
+    "reference_silent_thralls",
+    "reference_subsumed_captain",
+    "reference_mine_rescue_pressure",
+    "reference_ichor_demon_doctrine",
+    "reference_aftermath"
   ]
 };
--- data/campaigns/no-bell-for-harthrun/index.js
+++ data/campaigns/no-bell-for-harthrun/index.js
@@ -1,5 +1,7 @@
 import { campaignConfig } from "./campaign_config.js";
-import { currentLoadout } from "./current_loadout.js";
+import { currentLoadout } from "../current_loadout.js";
+
+import { arcConfig } from "./arcs/harthrun-investigation/arc_config.js";
 import { actors } from "./arcs/harthrun-investigation/actors.js";
 import { ambientCast } from "./arcs/harthrun-investigation/ambient_cast.js";
 import { factions } from "./arcs/harthrun-investigation/factions.js";
@@ -11,4 +13,114 @@ import { tables } from "./arcs/harthrun-investigation/tables.js";
 import { threads } from "./arcs/harthrun-investigation/threads.js";
 import { trackers } from "./arcs/harthrun-investigation/trackers.js";
 
-export const campaignData = { config: campaignConfig, currentLoadout, actors, ambientCast, factions, fireableMoments, locations, references, scenes, tables, threads, trackers };
+const harthrunInvestigationArc = {
+  config: arcConfig,
+  actors,
+  ambientCast,
+  factions,
+  fireableMoments,
+  locations,
+  references,
+  scenes,
+  tables,
+  threads,
+  trackers
+};
+
+const arcRegistry = {
+  [harthrunInvestigationArc.config.id]: harthrunInvestigationArc
+};
+
+export const availableArcs = Object.values(arcRegistry).map((arc) => ({
+  id: arc.config.id,
+  slug: arc.config.slug,
+  label: arc.config.label ?? arc.config.title ?? arc.config.name ?? arc.config.id,
+  name: arc.config.name ?? arc.config.title ?? arc.config.label ?? arc.config.id,
+  status: arc.config.status ?? "available",
+  summary: arc.config.summary ?? ""
+}));
+
+function normalizeArcId(arcId) {
+  if (arcRegistry[arcId]) return arcId;
+
+  const matchedArc = Object.values(arcRegistry).find((arc) => (
+    arc.config.slug === arcId ||
+    arc.config.name === arcId ||
+    arc.config.title === arcId ||
+    arc.config.label === arcId
+  ));
+
+  return matchedArc?.config.id ?? harthrunInvestigationArc.config.id;
+}
+
+function ids(items = []) {
+  return items.map((item) => item.id).filter(Boolean);
+}
+
+function currentLoadoutMatchesArc(arc) {
+  return (
+    currentLoadout.campaignId === campaignConfig.id &&
+    (
+      currentLoadout.arcId === arc.config.id ||
+      currentLoadout.arcId === arc.config.slug
+    )
+  );
+}
+
+function buildInspectionLoadout(arc) {
+  return {
+    ...currentLoadout,
+    campaignId: campaignConfig.id,
+    modeId: campaignConfig.modeId,
+    arcId: arc.config.id,
+    activeActors: ids(arc.actors),
+    activeLocations: ids(arc.locations),
+    activeAmbientCast: ids(arc.ambientCast),
+    activeFactions: ids(arc.factions),
+    activeThreads: ids(arc.threads),
+    activeTrackers: ids(arc.trackers),
+    activeScenes: ids(arc.scenes),
+    activeFireableMoments: ids(arc.fireableMoments),
+    activeTables: ids(arc.tables),
+    activeReferences: ids(arc.references)
+  };
+}
+
+function getEffectiveLoadout(arc) {
+  if (currentLoadoutMatchesArc(arc)) {
+    return {
+      activeAmbientCast: [],
+      activeFireableMoments: [],
+      ...currentLoadout,
+      campaignId: campaignConfig.id,
+      modeId: campaignConfig.modeId,
+      arcId: arc.config.id
+    };
+  }
+
+  return buildInspectionLoadout(arc);
+}
+
+export function getCampaignDataForArc(arcId = currentLoadout.arcId) {
+  const resolvedArcId = normalizeArcId(arcId);
+  const arc = arcRegistry[resolvedArcId] ?? harthrunInvestigationArc;
+
+  return {
+    config: campaignConfig,
+    activeArc: arc.config,
+    availableArcs,
+    currentLoadout: getEffectiveLoadout(arc),
+    actors: [...arc.actors],
+    locations: [...arc.locations],
+    ambientCast: [...arc.ambientCast],
+    fireableMoments: [...arc.fireableMoments],
+    factions: [...arc.factions],
+    threads: [...arc.threads],
+    trackers: [...arc.trackers],
+    scenes: [...arc.scenes],
+    tables: [...arc.tables],
+    references: [...arc.references]
+  };
+}
+
+export const campaignData = getCampaignDataForArc(currentLoadout.arcId);
